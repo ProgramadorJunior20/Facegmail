678e0113176c21d7161d8ea94956daf6052f7666
Jefferson Murillo <development@eticsas.co>
2022-02-28 18:23:57 -0500

    fix: Realice cambios en estos archivos

--- src/App.vue
+++ src/App.vue
@@ -6,39 +6,24 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-toolbar-title class="headline text-uppercase">
+          <span>Vue</span>
+        </v-toolbar-title>
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
+        <span class="mr-2">Cerrar sesión</span>
+        <v-icon class="fa-light">fa fas fa-address-book</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <v-container>
+        <router-view/>
+      </v-container>
     </v-main>
   </v-app>
 </template>
---
+++ src/views/Ingreso.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-row  class="d-flex justify-center mb-6 mt-5">
+        <v-col xs = "12" sm = "8" md = "6" xl = "4">
+            <v-card>
+                <v-card-text class="subtitle-1 text-uppercase primary white--text">
+                    <span>Ingreso</span>
+                </v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
